f7fbf9019f186a4d3fb05ad4797b6c87a2d375e6
Gungun Soni <gungunbps2018@gmail.com>
2025-07-20 13:28:07 +0530

    first commit

---
+++ src/data/initialData.js
@@ -0,0 +1,29 @@
+const initialData = {
+  "CSPM Executive Dashboard": [
+    {
+      id: "cloud-accounts",
+      name: "Cloud Accounts",
+      content: "Random content for Cloud Accounts"
+    },
+    {
+      id: "cloud-risk",
+      name: "Cloud Account Risk Assessment",
+      content: "Random content for Risk Assessment"
+    }
+  ],
+  "CWPP Dashboard": [],
+  "Registry Scan": [
+    {
+      id: "image-risk",
+      name: "Image Risk Assessment",
+      content: "Random content for Image Risk"
+    },
+    {
+      id: "image-security",
+      name: "Image Security Issues",
+      content: "Random content for Image Security"
+    }
+  ]
+};
+
+export default initialData;
